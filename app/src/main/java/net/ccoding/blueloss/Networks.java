8762bc5cadc061f45d9bd92969446914b987b7c9
Coop <coop.coding@gmail.com>
2018-08-07 13:11:50 +1000

    Added token type when saving gson network data.

--- app/src/main/java/net/ccoding/blueloss/Networks.java
+++ app/src/main/java/net/ccoding/blueloss/Networks.java
@@ -8,7 +8,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 final class Networks {
-  private static String toastMessageForNullBSSID = "The current network returned null for the bssid, so we're not saving it :-(";
+  private static String toastMessageForNullBSSID = "The current network returned null for the bssid, so we're not saving it. Sorry :-(";
+  // https://stackoverflow.com/a/12117517/2785644
+  private static Type typeOfHashMap = new TypeToken<Map<String, String>>() { }.getType();
 
   public static boolean isConnectedToASavedNetwork() {
     Map.Entry<String,String> networkInfo = Utils.getStringMapFirstEntry(NetworkInfo.getNetworkInfo());
@@ -18,24 +20,6 @@ final class Networks {
       return false;
     }
     return networkFoundInSavedNetworks(bssid);
-  }
-
-  private static boolean networkFoundInSavedNetworks(String bssid){
-    Map<String,String> savedNetworks = getSavedNetworks();
-    return savedNetworks.containsKey(bssid);
-  }
-
-  private static Map<String,String> getSavedNetworks(){
-    String serializedNetworks = MainActivity.prefsNetworks.getString("networks","");
-    // https://stackoverflow.com/a/12117517/2785644
-    Type typeOfHashMap = new TypeToken<Map<String, String>>() { }.getType();
-    Gson gson = new Gson();
-    Map<String,String> networks = gson.fromJson(serializedNetworks, typeOfHashMap);
-
-    if(networks == null){
-      networks = new HashMap<>();
-    }
-    return networks;
   }
 
   public static void saveCurrentNetwork(){
@@ -49,6 +33,29 @@ final class Networks {
     saveNetwork(bssid, ssid);
   }
 
+  public static void removeNetwork(String bssid){
+    if(!networkFoundInSavedNetworks(bssid)){
+      return;
+    }
+    Map<String,String> savedNetworks = getSavedNetworks();
+    savedNetworks.remove(bssid);
+
+    MainActivity.prefsNetworks.edit().putString(
+        "networks",
+        convertNetworksHashMapToJsonString(savedNetworks)
+    ).apply();
+  }
+
+  private static Map<String,String> getSavedNetworks(){
+    String serializedNetworks = MainActivity.prefsNetworks.getString("networks","");
+    Map<String,String> networks = convertJsonStringToNetworksHashMap(serializedNetworks);
+
+    if(networks == null){
+      networks = new HashMap<String,String>();
+    }
+    return networks;
+  }
+
   private static void saveNetwork(String bssid, String ssid){
     if(networkFoundInSavedNetworks(bssid)){
       return;
@@ -59,25 +66,22 @@ final class Networks {
 
     MainActivity.prefsNetworks.edit().putString(
         "networks",
-        convertNetworksMapToJsonString(savedNetworks)
+        convertNetworksHashMapToJsonString(savedNetworks)
     ).apply();
   }
 
-  public static void removeNetwork(String bssid){
-    if(!networkFoundInSavedNetworks(bssid)){
-      return;
-    }
+  private static boolean networkFoundInSavedNetworks(String bssid){
     Map<String,String> savedNetworks = getSavedNetworks();
-    savedNetworks.remove(bssid);
-
-    MainActivity.prefsNetworks.edit().putString(
-        "networks",
-        convertNetworksMapToJsonString(savedNetworks)
-    ).apply();
+    return savedNetworks.containsKey(bssid);
   }
 
-  private static String convertNetworksMapToJsonString(Map<String,String> networks){
+  private static Map<String,String> convertJsonStringToNetworksHashMap(String json){
     Gson gson = new Gson();
-    return gson.toJson(networks);
+    return gson.fromJson(json, typeOfHashMap);
+  }
+
+  private static String convertNetworksHashMapToJsonString(Map<String,String> networks){
+    Gson gson = new Gson();
+    return gson.toJson(networks, typeOfHashMap);
   }
 }
